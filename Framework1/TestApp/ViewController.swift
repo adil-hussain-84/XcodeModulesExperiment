36a6a4cc068b866c8d7de96baa746c67a08b2ac2
Adil Hussain <adilson05uk@gmail.com>
2021-07-13 15:24:43 +0100

    Added "TestApp" application target within the "Framework1" project.

---
+++ Framework1/TestApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Adil Hussain on 13/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
